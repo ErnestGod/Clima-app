c964d94d958352721fcd4d3a02717f71b6017e74
ErnestGod <e.god465@gmail.com>
2018-05-09 23:55:01 +0200

    Initial Commit

---
+++ Clima-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clima-App
+//
+//  Created by ErnestG on 09.05.2018.
+//  Copyright © 2018 ErnestG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
